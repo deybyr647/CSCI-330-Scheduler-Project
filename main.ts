d4f417c0f8293d5134a6fe6d7288b097391cd479
deybyr647 <deybyr647@gmail.com>
2023-04-17 14:50:23 -0400

    Basic testing code

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 import Process from "./src/Process";
+import Queue from "./src/Queue";
 
-const process1 = new Process("1", 0, 5);
+const process1: Process = new Process("1", 0, 5);
 
 console.log(process1.getPID());
 console.log(process1);
+
+const processQueue: Queue<Process> = new Queue<Process>();
+processQueue.enqueue(process1);
+processQueue.print();
+
+processQueue.fromArray([process1, process1, process1] as Process[]);
+processQueue.print();
